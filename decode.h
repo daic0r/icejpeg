bae982d2a96b1cff08e4f50d92bb7d3566d11901
Matthias Gruen <matthias@Matthiass-iMac.local>
2016-01-11 19:18:19 +0100

    More cleanup work

---
+++ decode.h
@@ -0,0 +1,13 @@
+//
+//  decode.h
+//  icejpeg
+//
+//  Created by Matthias Gruen on 11/01/16.
+//  Copyright © 2016 Matthias Gruen. All rights reserved.
+//
+
+#ifndef decode_h
+#define decode_h
+
+
+#endif /* decode_h */
---
+++ main.c
@@ -0,0 +1,9 @@
+//
+//  main.c
+//  icejpeg
+//
+//  Created by Matthias Gruen on 11/01/16.
+//  Copyright © 2016 Matthias Gruen. All rights reserved.
+//
+
+#include <stdio.h>
