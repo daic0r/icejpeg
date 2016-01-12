bd71de55a8f0c36aa98ca1b31acdcb438e534370
Matthias Gruen <matthias@Matthiass-iMac.local>
2016-01-12 16:48:10 +0100

    added DCT.h

---
+++ DCT.h
@@ -0,0 +1,17 @@
+//
+//  DCT.h
+//  icejpeg
+//
+//  Created by Matthias Gruen on 11/01/16.
+//  Copyright © 2016 Matthias Gruen. All rights reserved.
+//
+
+#ifndef DCT_h
+#define DCT_h
+
+#define INT32 int
+#define DCTELEM INT32
+
+void fdct(DCTELEM * data);
+
+#endif /* DCT_h */
